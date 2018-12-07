dd70cbdb610c373b0243981af621cf6be0dda85f
TonyvanderKrogt <38463912+TonyvanderKrogt@users.noreply.github.com>
2018-12-07 18:18:51 +0100

    Ball bounce update

--- ball.cpp
+++ ball.cpp
@@ -2,7 +2,11 @@
 
 ball::ball(sf::Vector2f position, float size) :
 	position( position ),
-	size( size )
+	size( size ),
+	topCircle(sf::Vector2f{ (size * 2/3),size }),
+	botCircle(sf::Vector2f{ (size * 2/3),size }),
+	leftCircle(sf::Vector2f{ size,(size * 2/3) }),
+	rightCircle(sf::Vector2f{ size,(size * 2/3) })
 {}
 
 void ball::draw(sf::RenderWindow & window) const {
@@ -11,79 +15,61 @@ void ball::draw(sf::RenderWindow & window) const {
 	circle.setPosition(position);
 	circle.setFillColor(sf::Color::Blue);
 	window.draw(circle);
+	window.draw(topCircle);
+	window.draw(leftCircle);
+	window.draw(botCircle);
+	window.draw(rightCircle);
 }
 
 void ball::move(sf::Vector2f delta) {
 	position += delta;
+	hitbox_update();
 }
 
-bool ball::BallIntersects(sf::RectangleShape rect, float &xMovBall, float& yMovBall)
+bool ball::BallIntersects(sf::RectangleShape player, float &xMovBall, float& yMovBall)
 {
 	sf::CircleShape circle;
 	circle.setPosition(position);
 	circle.setRadius(size);
-	sf::FloatRect rectObj = rect.getGlobalBounds();
+	sf::FloatRect rectObj = player.getGlobalBounds();
+	sf::FloatRect top = topCircle.getGlobalBounds();
+	sf::FloatRect bot = botCircle.getGlobalBounds();
+	sf::FloatRect left = leftCircle.getGlobalBounds();
+	sf::FloatRect right = rightCircle.getGlobalBounds();
 	sf::FloatRect circleObj = circle.getGlobalBounds();
-	if (circleObj.intersects(rectObj)) {
-		sf::Vector2f topCircle(circleObj.left+ (circleObj.width / 2), circleObj.top);
-		sf::Vector2f rightCircle(circleObj.left + circleObj.width, circleObj.top + (circleObj.height / 2));
-		sf::Vector2f botCircle(circleObj.left + (circleObj.width/2), circleObj.top + circleObj.height);
-		sf::Vector2f leftCircle(circleObj.left, circleObj.top + (circleObj.height / 2));
-		
-		// topside of player 
-		if (botCircle.y > rectObj.top
-			&& botCircle.y < rectObj.top + rectObj.height
-			&& botCircle.x > rectObj.left 
-			&& botCircle.x < rectObj.left + rectObj.width )
-		{
-			std::cout << "TOP "<< botCircle.y << "\n\n";
-			yMovBall *= -1;
-		}	
-		// bottomside of player
-		else if (topCircle.y < rectObj.top +rectObj.height
-			&& topCircle.y > rectObj.top
-			&& topCircle.x > rectObj.left 
-			&& topCircle.x < rectObj.left + rectObj.width )
-		{
-			std::cout << "BOT\n\n";
-			yMovBall *= -1;
-		}
-		// rightside of player
-		else if (leftCircle.x > rectObj.left 
-			&& leftCircle.x < rectObj.left + rectObj.width
-			&& leftCircle.y > rectObj.top 
-			&& leftCircle.y < rectObj.top + rectObj.height 
-			)
-		{
-			std::cout << "RIGHT\n\n";
-			xMovBall *= -1;
-		}
-		// leftside of player
-		else if (rightCircle.x > rectObj.left 
-			&& rightCircle.x < rectObj.left + rectObj.width
-			&& rightCircle.y > rectObj.top 
-			&& rightCircle.y < rectObj.top + rectObj.height 
-			)
-		{
-			std::cout << "LEFT\n\n";
-			xMovBall *= -1;
-		}
-		else {
-			std::cout << "CORNERS\n\n";
-			if (botCircle.y + 1 > rectObj.top || topCircle.y - 1 < rectObj.top + rectObj.height) {
-				yMovBall *= -1;
-			}
-			else if (rightCircle.x + 1 > rectObj.left || leftCircle.x - 1 < rectObj.left + rectObj.width) {
-				xMovBall *= -1;
-			}
-			else {
-				xMovBall *= -1;
-				yMovBall *= -1;
-			}
-		}
-		
+
+	if (top.intersects(rectObj)){
+		yMovBall = 3 ;
+	}
+	if (bot.intersects(rectObj)) {
+		yMovBall = -3;
 	}
 
-	return (circleObj.intersects(rectObj));
+	if (left.intersects(rectObj)) {
+		xMovBall = 3;
+	}
+	if (right.intersects(rectObj)) {
+		xMovBall = -3;
+	}
+
+	return (circleObj.intersects(rectObj) || top.intersects(rectObj) || bot.intersects(rectObj) || left.intersects(rectObj) || right.intersects(rectObj));
+}
+
+void ball::hitbox_update()
+{
+	topCircle.setPosition(position.x + (size * 2 / 3), position.y);
+	leftCircle.setPosition(position.x, position.y + (size * 2 / 3));
+	botCircle.setPosition(position.x + (size * 2 / 3), position.y + size);
+	rightCircle.setPosition(position.x + size, position.y + (size * 2 / 3));
+	color_hitboxes();
+}
+
+void ball::color_hitboxes()
+{
+	topCircle.setFillColor(sf::Color::Yellow);
+	leftCircle.setFillColor(sf::Color::Green);
+	botCircle.setFillColor(sf::Color::Red);
+	rightCircle.setFillColor(sf::Color::White);
+
 }
 
--- ball.hpp
+++ ball.hpp
@@ -14,9 +14,18 @@ public:
 	void move(sf::Vector2f delta)  override;
 
 	bool BallIntersects(sf::RectangleShape rect, float& xMovBall, float& yMovBall);
+
+	void hitbox_update();
+
+	void color_hitboxes();
 private:
+
 	sf::Vector2f position;
 	float size;
+	sf::RectangleShape topCircle;
+	sf::RectangleShape botCircle;
+	sf::RectangleShape leftCircle;
+	sf::RectangleShape rightCircle;
 };
 
 #endif
